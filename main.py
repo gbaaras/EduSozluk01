38d034c9945a99b4d7d459aebb4024e19b0e808d
Gökay Burak AKKUŞ <gokayakkus@araskargo.com.tr>
2024-02-08 20:07:07 +0300

    yeni yazı ekleme

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import pymongo
 app = Flask(__name__)
+app.secret_key = 'cok gizli super secret key'
 
 # MongoDB'ye bağlantı kur
 client = pymongo.MongoClient()
 db = client["EduSozlukDB"]
+
+
+def get_sequence(seq_name):
+    return db.counters.find_one_and_update(filter={"_id": seq_name}, update={"$inc": {"seq": 1}}, upsert=True)["seq"]
 
 
 @app.route('/hello/<name>')
@@ -14,7 +19,32 @@ def hello_name(name):
 
 @app.route('/')
 def home_page():
-    return render_template("home.html")
+    basliklar = list(db["basliklar"].find({}))
+    print("ilk başlık:",basliklar[0])
+    yazilar = list(db["yazilar"].find({"baslik_id": basliklar[0]["_id"]}))
+    return render_template("baslik.html",aktif_baslik=basliklar[0], basliklar=basliklar, yazilar=yazilar)
+
+
+@app.route('/baslik/<baslik_id>')
+def baslik_goster(baslik_id):
+    if request.method == 'GET':
+        basliklar = list(db["basliklar"].find({}))
+        aktif_baslik = db["basliklar"].find_one({"_id": int(baslik_id)})
+        yazilar = list(db["yazilar"].find({"baslik_id": int(baslik_id) }))
+        return render_template("baslik.html", aktif_baslik=aktif_baslik, basliklar=basliklar, yazilar=yazilar)
+
+
+@app.route('/yazi-ekle', methods=["POST"])
+def yazi_ekle():
+    if request.method == 'POST':
+        baslik_id = request.form["baslik_id"]
+        db["yazilar"].insert_one({
+            "_id": get_sequence("yazilar"),
+            "baslik_id": int(baslik_id),
+            "yazi": request.form["yeni_yazi"]
+        })
+
+        return redirect("/baslik/"+baslik_id, 302)
 
 
 @app.route('/uye-ol', methods=["GET", "POST"])
@@ -51,11 +81,16 @@ def giris():
         kullanici = db["kullanicilar"].find_one({"_id": email})
         print("kullanici:",kullanici)
         if kullanici and kullanici["sifre"] == sifre:
+            session['kullanici'] = kullanici
             return redirect("/", 302)
         else:
             return "Kullanıcı bulunamadı ya da şifre geçersiz"
 
 
+@app.route('/cikis', methods=["GET", "POST"])
+def cikis():
+    session.pop('kullanici', None)
+    return redirect("/", 302)
 
 
 if __name__ == '__main__':
